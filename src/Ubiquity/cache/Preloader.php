6ad661a757d541cb85c74d24990f09c289c81b02
jcheron <myaddressmail@gmail.com>
2019-12-20 16:12:36 +0100

    Add javadoc

--- src/Ubiquity/cache/Preloader.php
+++ src/Ubiquity/cache/Preloader.php
@@ -13,7 +13,8 @@ class Preloader {
 
 	private $vendorDir;
 
-	private $libraries = [
+	private static $libraries = [
+		'application' => './../app/',
 		'ubiquity' => 'phpmv/ubiquity/src/Ubiquity/',
 		'ubiquity-dev' => 'phpmv/ubiquity-dev/src/Ubiquity/',
 		'ubiquity-webtools' => 'phpmv/ubiquity-webtools/src/Ubiquity/',
@@ -33,11 +34,23 @@ class Preloader {
 
 	private $loader;
 
+	/**
+	 * Creates a new loader instance for this application.
+	 *
+	 * @param string $appRoot
+	 *        	The app root
+	 */
 	public function __construct($appRoot) {
 		$this->vendorDir = $appRoot . './../vendor/';
 		$this->loader = require $this->vendorDir . 'autoload.php';
 	}
 
+	/**
+	 * Adds paths to be scanned during preloading.
+	 *
+	 * @param string ...$paths
+	 * @return Preloader
+	 */
 	public function paths(string ...$paths): Preloader {
 		foreach ($paths as $path) {
 			$this->addDir($path);
@@ -45,12 +58,24 @@ class Preloader {
 		return $this;
 	}
 
+	/**
+	 * Adds namespaces to exclude from preloading.
+	 *
+	 * @param string ...$names
+	 * @return Preloader
+	 */
 	public function exclude(string ...$names): Preloader {
 		$this->excludeds = \array_merge($this->excludeds, $names);
 		return $this;
 	}
 
-	public function addClass($class): bool {
+	/**
+	 * Adds a class to preload.
+	 *
+	 * @param string $class
+	 * @return bool
+	 */
+	public function addClass(string $class): bool {
 		if (! $this->isExcluded($class)) {
 			if (! isset($this->classes[$class])) {
 				$path = $this->getPathFromClass($class);
@@ -63,12 +88,20 @@ class Preloader {
 		return false;
 	}
 
+	/**
+	 * Adds an array of classes to preload.
+	 *
+	 * @param array $classes
+	 */
 	public function addClasses(array $classes) {
 		foreach ($classes as $class) {
 			$this->addClass($class);
 		}
 	}
 
+	/**
+	 * Preload all added classes.
+	 */
 	public function load() {
 		foreach ($this->classes as $class => $file) {
 			if (! $this->isExcluded($class)) {
@@ -77,7 +110,12 @@ class Preloader {
 		}
 	}
 
-	public function generateClassesFiles() {
+	/**
+	 * Returns a generated associative array of classes to preload (key: class, value: file).
+	 *
+	 * @return array
+	 */
+	public function generateClassesFiles(): array {
 		$ret = [];
 		foreach ($this->classes as $class => $file) {
 			if (! $this->isExcluded($class)) {
@@ -87,7 +125,14 @@ class Preloader {
 		return $ret;
 	}
 
-	public function generateToFile($filename, $preserve = true) {
+	/**
+	 * Generate a file containing the associative array of classes to preload (classes-files=>[key: class, value: file}).
+	 *
+	 * @param string $filename
+	 * @param ?bool $preserve
+	 * @return int
+	 */
+	public function generateToFile(string $filename, ?bool $preserve = true): int {
 		$array = [];
 		if ($preserve && \file_exists($filename)) {
 			$array = include $filename;
@@ -97,7 +142,13 @@ class Preloader {
 		return \file_put_contents($filename, $content);
 	}
 
-	public function addDir($dirname) {
+	/**
+	 * Adds a directory to be scanned during preloading.
+	 *
+	 * @param string $dirname
+	 * @return \Ubiquity\cache\Preloader
+	 */
+	public function addDir(string $dirname): Preloader {
 		$files = $this->glob_recursive($dirname . DIRECTORY_SEPARATOR . '*.php');
 		foreach ($files as $file) {
 			$class = $this->getClassFullNameFromFile($file);
@@ -108,58 +159,139 @@ class Preloader {
 		return $this;
 	}
 
-	public function addLibraryPart($library, $part = ''): bool {
-		if (isset($this->libraries[$library])) {
-			$dir = $this->vendorDir . $this->libraries[$library] . $part;
+	/**
+	 * Adds a part of an existing library to be preloaded.
+	 * The available libraries can be obtained with the getLibraries method.
+	 *
+	 * @param string $library
+	 * @param ?string $part
+	 * @return bool
+	 */
+	public function addLibraryPart(string $library, ?string $part = ''): bool {
+		if (isset(self::$libraries[$library])) {
+			$dir = $this->vendorDir . self::$libraries[$library] . $part;
 			if (\file_exists($dir)) {
-				$this->addDir($this->vendorDir . $this->libraries[$library] . $part);
+				$this->addDir($dir);
 				return true;
 			}
 		}
 		return false;
 	}
 
+	/**
+	 * Adds Ubiquity framework controller and routing classes preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityControllers() {
 		$this->addLibraryPart('ubiquity', 'controllers');
 		return $this;
 	}
 
+	/**
+	 * Adds Ubiquity framework cache system classes to preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityCache() {
 		$this->addLibraryPart('ubiquity', 'cache');
 		return $this;
 	}
 
+	/**
+	 * Adds Ubiquity framework PDO classes to preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityPdo() {
 		$this->addClass('Ubiquity\\db\\Database');
 		$this->addLibraryPart('ubiquity', 'db/providers');
 		return $this;
 	}
 
+	/**
+	 * Adds Ubiquity framework ORM classes to preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityORM() {
 		$this->addLibraryPart('ubiquity', 'orm');
 		return $this;
 	}
 
+	/**
+	 * Adds Ubiquity framework Http classes to preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityHttpUtils() {
 		$this->addLibraryPart('ubiquity', 'utils/http');
 		return $this;
 	}
 
+	/**
+	 * Adds Ubiquity framework MicroTemplateEngine classes to preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityViews() {
 		$this->addClass('Ubiquity\\views\\engine\\micro\\MicroTemplateEngine');
 		return $this;
 	}
 
+	/**
+	 * Adds Ubiquity framework Translations classes to preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityTranslations() {
 		$this->addLibraryPart('ubiquity', 'translation');
 		return $this;
 	}
 
+	/**
+	 * Adds Ubiquity-workerman classes to preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityWorkerman() {
 		$this->addLibraryPart('ubiquity-workerman');
 		return $this;
 	}
 
+	/**
+	 * Adds classes from an application part (app folder) to preload.
+	 *
+	 * @param string $part
+	 * @return boolean
+	 */
+	public function addApplicationPart(?string $part = '') {
+		return $this->addLibraryPart('application', $part);
+	}
+
+	/**
+	 *
+	 * @param string $dir
+	 * @return boolean
+	 */
+	public function addApplicationModels($dir = 'models') {
+		return $this->addLibraryPart('application', $dir);
+	}
+
+	/**
+	 *
+	 * @param string $dir
+	 * @return boolean
+	 */
+	public function addApplicationControllers($dir = 'controllers') {
+		return $this->addLibraryPart('application', $dir);
+	}
+
+	/**
+	 * Adds Twig templating system classes to preload.
+	 *
+	 * @return \Ubiquity\cache\Preloader
+	 */
 	public function addUbiquityTwig() {
 		$this->addClasses([
 			'Ubiquity\\views\\engine\\Twig',
@@ -175,6 +307,13 @@ class Preloader {
 		return $this;
 	}
 
+	/**
+	 * Defines classes to be preloaded in a file returning an associative array keys : (classes-files, excludeds, paths, classes, libraries-parts, callback).
+	 *
+	 * @param string $appRoot
+	 * @param string $filename
+	 * @return bool
+	 */
 	public static function fromFile(string $appRoot, string $filename): bool {
 		if (\file_exists($filename)) {
 			$array = include $filename;
@@ -183,6 +322,13 @@ class Preloader {
 		return false;
 	}
 
+	/**
+	 * Defines classes to be preloaded with an associative array keys : (classes-files, excludeds, paths, classes, libraries-parts, callback).
+	 *
+	 * @param string $appRoot
+	 * @param array $array
+	 * @return bool
+	 */
 	public static function fromArray(string $appRoot, array $array): bool {
 		$pre = new self($appRoot);
 		self::$count = 0;
@@ -217,6 +363,15 @@ class Preloader {
 		}
 		$pre->load();
 		return self::$count > 0;
+	}
+
+	/**
+	 * Returns an array of available libraries to be preloaded
+	 *
+	 * @return array
+	 */
+	public static function getLibraries() {
+		return \array_keys(self::$libraries);
 	}
 
 	private function addClassFile($class, $file) {
